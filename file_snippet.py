fed09f4131e933f3afc6965d37188960ae85af17
“TOKB27” <sa122moshita@gmail.com>
2024-01-09 21:38:48 +0900

    ファイル操作の追加

--- file_snippet.py
+++ file_snippet.py
@@ -1,5 +1,60 @@
 ### ファイル操作スニペット ###
 
+import os
+import pathlib
+import glob
+import shutil
+
+
+# ファイル作成
+print('file_test.txt作成します。')
+pathlib.Path('file_test.txt').touch()
+if os.path.exists('file_test.txt'):
+  print('exist file_test.txt')
+else:
+  print('not exist file_test.txt')
+
+# ファイル削除
+print('file_test.txt削除します。')
+os.remove('file_test.txt')
+if os.path.exists('file_test.txt'):
+  print('exist file_test.txt')
+else:
+  print('not exist file_test.txt')
+
+# ディレクトリ作成
+print('test.dir作成します。')
+os.mkdir('test_dir')
+if os.path.exists('test_dir'):
+  print('exist test.dir')
+else:
+  print('not exist test_dir')
+  
+# ディレクトリ削除
+print('test.dir削除します。')
+os.rmdir('test_dir')
+if os.path.exists('test_dir'):
+  print('exist test.dir')
+else:
+  print('not exist test_dir')
+
+# リネーム
+print('リネームでrename.txtを作成します')
+pathlib.Path('file_test.txt').touch()
+os.rename('file_test.txt', 'rename.txt')
+if os.path.exists('rename.txt'):
+  print('exist rename.txt')
+os.remove('rename.txt')
+
+# シンボリックリンク
+print('シンボリックリンクでsymlink.txtを作成します')
+pathlib.Path('file_test.txt').touch()
+os.symlink('file_test.txt', 'symlink.txt')
+if os.path.exists('symlink.txt'):
+  print('exist symlink.txt')
+os.unlink('symlink.txt')
+os.remove('file_test.txt')
+
 # ファイルの書き込み
 f = open('file.txt', 'w')
 f.write('ファイルの書き込み\n')
